7a01e36f7ef58871762802464a4db683a3c6d1cc
AF <amfraine@gmail.com>
2020-04-22 22:27:49 -0400

    several changes in organization

--- run.py
+++ run.py
@@ -1,52 +1,80 @@
 import requests
 from datetime import date, datetime
+import sys
+import json
 
-def setup_headers(rapid_api_key):
-    headers_cb = {
+def get_todays_funding_announcements(key, num):
+    headers = {
         'x-rapidapi-host': "crunchbase-crunchbase-v1.p.rapidapi.com",
-        'x-rapidapi-key': rapid_api_key
+        'x-rapidapi-key': key
     }
-    headers_automata = {
-        'x-rapidapi-host': "market-intelligence-by-automata.p.rapidapi.com",
-        'x-rapidapi-key': rapid_api_key
-    }
-    return (headers_cb, headers_automata)
-
-def get_todays_funding_announcements():
+    
+    timestamp = int(datetime.combine(date.today(), datetime.min.time()).timestamp())
     querystring = {"updated_since": timestamp}
+    
     url_cb_funding = "https://crunchbase-crunchbase-v1.p.rapidapi.com/funding-rounds"
 
-    funding_response = requests.request("GET", url_cb_funding, headers=headers_cb, params=querystring)
-    funding_rounds = funding_response.get("data").get("items")
-    funding_round_uuid = [x.get("properties").get("permalink") for x in funding_rounds]
+    response = requests.request("GET", url_cb_funding, headers=headers, params=querystring)
+    funding_rounds = [x.get("properties") for x in response.json().get("data").get("items")]
+    
+    funding_uids = [ x.get("permalink") for x in funding_rounds if x.get("funding_type") == "seed" ]
 
-    return funding_round_uuid
-
-def get_funding_recipients(funding_uuids):
-    url_cb_details = ["https://crunchbase-crunchbase-v1.p.rapidapi.com/funding-rounds/{}".format(x) for x in funding_uuids]
+    url_cb_details = [ url_cb_funding + "/{}".format(x) for x in funding_uids ]
 
     funded_companies = []
-    for url_ in url_details:
-        company_response = requests.request("GET", url_cb_details, headers=headers_cb)
-        funded_companies.push(company_response.get("data").get("relationships").get("funded_organizations").get("item").get("properties")
+    for url_ in url_cb_details[:num]:
+        company_response = requests.request("GET", url_, headers=headers).json()
+        r = company_response.get("data").get("relationships")
+        o = r.get("funded_organization").get("item").get("properties")
+        funded_companies.append(o)
 
-    funded_company_details = [{"website": x.get("website"), "description": x.get("description") } for x in funded_companies]
+    return [ {
+        "website": x.get("homepage_url"), 
+        "description": x.get("description") 
+    } for x in funded_companies ]
 
-    return funded_company_details
-
-def get_similar_companies_with_website(company_details):
+def get_company_lookalikes(funded_company_details, key):
+    headers = {
+        'x-rapidapi-host': "market-intelligence-by-automata.p.rapidapi.com",
+        'x-rapidapi-key': key
+    }
     url_automata_similar = "https://market-intelligence-by-automata.p.rapidapi.com/similar"
     query_strings = [{"page":"0","link":x.get("website")} for x in funded_company_details]
-    similar_companies = []
+    company_lookalikes = []
     for s in query_strings:
-        similar_companies_response = requests.request("GET", url_automata_similar, headers=headers_automata, params=s)
-        similar_companies.push({
+        response = requests.request("GET", url_automata_similar, headers=headers, params=s)
+        company_lookalikes.append({
             "input_company": s.get("link"),
-            "similar_companies": similar_companies_response.get("companies")
+            "similar_companies": response.json().get("companies")
         })
-    return similar_companies
+    return company_lookalikes
 
+def main(argv):
+    if '-k' in sys.argv:
+        key_index = [x for x in range(len(sys.argv)) if sys.argv[x] == "-k"][0]
+        if len(sys.argv) >= key_index + 2:
+            rapid_api_key = sys.argv[key_index + 1]
+        else:
+            print("no rapid api key")
+            sys.exit(2)
+    if '-max' in sys.argv:
+        max_index = [x for x in range(len(sys.argv)) if sys.argv[x] == "-max"][0]
+        if len(sys.argv) >= max_index + 2:
+            max_num = int(sys.argv[max_index + 1])
+        else:
+           max_num = 5
+    else:
+        max_num = 5
+   
+    funding_recipients = get_todays_funding_announcements(rapid_api_key, max_num)
+    company_lookalikes = get_company_lookalikes(funding_recipients, rapid_api_key)
+    with open('company_lookalikes_{}.json'.format(int(datetime.now().timestamp())), 'w') as outfile:
+        json.dump(company_lookalikes, outfile)
+    print("Done!")
 
+if __name__ == "__main__":
+    main(sys.argv[1:])
+    
 
 
 
